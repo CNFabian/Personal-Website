f42f97056bc6bf54e59cabca6a3f584f892cf9a3
CNFabian <cfabian2127@gmail.com>
2024-06-22 02:18:48 -0700

    Added Information, no images

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
+document.getElementById('recommendationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const message = document.getElementById('message').value;
+
+    if (name && message) {
+        const recommendationSection = document.getElementById('recommendations');
+        
+        const newRecommendation = document.createElement('div');
+        newRecommendation.className = 'recommendation';
+        newRecommendation.innerHTML = `<h4><i>${name}</i></h4>
+                <p><i>${message}</i></p>`;
+        
+        recommendationSection.insertBefore(newRecommendation, recommendationSection.querySelector('.recommendation-form'));
+        
+        document.getElementById('recommendationForm').reset();
+        submitRecommendation();
+    }
+});
+
 function submitRecommendation() {
     // Simulate submitting a recommendation and showing the popup
     document.getElementById('popup').classList.add('visible');
